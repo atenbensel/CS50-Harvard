bcaf31a0b1d7758610887dc45331c4646d609ca3
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2024-05-25 10:49:46 -0500

    placeholder for readability.c

---
+++ Problem Set 2/readability.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Prompt the user for some text
+
+    // Count the number of letters, words, and sentences in the text
+
+    // Compute the Coleman-Liau index
+
+    // Print the grade level
+}
