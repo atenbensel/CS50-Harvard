3dc2015310b77f53ab1476214563a50185ef928d
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2024-05-25 11:43:22 -0500

    adding readability.c

--- Problem Set 2/readability.c
+++ Problem Set 2/readability.c
@@ -4,13 +4,79 @@
 #include <stdio.h>
 #include <string.h>
 
+// Function prototypes
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
 int main(void)
 {
     // Prompt the user for some text
+    string text = get_string("Text: ");
 
     // Count the number of letters, words, and sentences in the text
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
 
     // Compute the Coleman-Liau index
+    float L = (float)letters / words * 100;
+    float S = (float)sentences / words * 100;
+    int index = round(0.0588 * L - 0.296 * S - 15.8); // Calculates Coleman-Liau index
 
     // Print the grade level
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}
+
+// Count letters in a string
+int count_letters(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i])) // check if char is alphabetic
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+// Count words in a string
+int count_words(string text)
+{
+    int count = 1;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == ' ') // Check spaces to see if letters are separate words
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+// Count sentences in a string
+int count_sentences(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            count++;
+        }
+    }
+    return count;
 }
